65b47bf474a957b74284e5d635209edc8c1e7b15
ChicRock <jungsungrak@gmail.com>
2018-04-28 13:15:00 +0900

    Move Feed navigationOptions to route

--- routes/HomeRoute.js
+++ routes/HomeRoute.js
@@ -1,16 +1,29 @@
+import React from "react";
+import { Image } from "react-native";
 import { StackNavigator } from "react-navigation";
 import FeedScreen from "../screens/FeedScreen";
 import sharedRoutes, { sharedOptions } from "./sharedRoutes";
+import NavButton from "../components/NavButton";
 
 const HomeRoute = StackNavigator(
   {
     Home: {
       screen: FeedScreen,
-      /*
-      navigationOptions: {
-        title: "Nomadgram",
-      },
-      */
+      navigationOptions: ({ navigation }) => ({
+        headerTitle: (
+          <Image
+            source={require("../assets/images/logo.png")}
+            style={{ height: 35 }}
+            resizeMode={"contain"}
+          />
+        ),
+        headerLeft: (
+          <NavButton
+            iconName={"ios-camera-outline"}
+            onPress={() => navigation.navigate("TakePhoto")}
+          />
+        ),
+      }),
     },
     ...sharedRoutes,
   },
--- screens/FeedScreen/container.js
+++ screens/FeedScreen/container.js
@@ -4,21 +4,6 @@ import { Image } from "react-native";
 import NavButton from "../../components/NavButton";
 
 class Container extends Component {
-  static navigationOptions = ({ navigation }) => ({
-    headerTitle: (
-      <Image
-        source={require("../../assets/images/logo.png")}
-        style={{ height: 35 }}
-        resizeMode={"contain"}
-      />
-    ),
-    headerLeft: (
-      <NavButton
-        iconName={"ios-camera-outline"}
-        onPress={() => navigation.navigate("TakePhoto")}
-      />
-    ),
-  });
   render() {
     return <FeedScreen {...this.props} />;
   }
