97e62b430338a7cfc30b82d540fd368a387206a9
ChicRock <jungsungrak@gmail.com>
2018-04-29 17:30:31 +0900

    Create FeedScreen with ScrollView and RefreshConrol

--- constants.js
+++ constants.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://d4e7f956.ngrok.io";
+export const API_URL = "http://ad016fcc.ngrok.io";
 export const FACEBOOK_APP_ID = "176594353126012";
--- screens/FeedScreen/presenter.js
+++ screens/FeedScreen/presenter.js
@@ -1,10 +1,36 @@
 import React from "react";
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  ScrollView,
+  RefreshControl,
+  StyleSheet,
+} from "react-native";
+import PropTypes from "prop-types";
 
 const FeedScreen = props => (
-  <View>
-    <Text onPress={() => props.navigation.navigate("Likes")}>Feed Screen</Text>
-  </View>
+  <ScrollView
+    refreshControl={
+      <RefreshControl
+        refreshing={props.isFetching}
+        onRefresh={props.refresh}
+        tintColor={"black"}
+      />
+    }
+    contentContainerStyle={styles.container}
+  />
 );
+
+FeedScreen.propTypes = {
+  refresh: PropTypes.func.isRequired,
+  isFetching: PropTypes.bool.isRequired,
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+});
 
 export default FeedScreen;
